374c7711c43fd48acc28182c03ac0a9de4a46f56
Suneet <akashrout>
2019-09-25 16:56:20 +0530

    Log4j Properties file added

--- emaillicence/src/main/java/com/licence/emaillicence/EmaillicenceApplication.java
+++ emaillicence/src/main/java/com/licence/emaillicence/EmaillicenceApplication.java
@@ -1,13 +1,20 @@
 package com.licence.emaillicence;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EmaillicenceApplication {
 
+	static Logger log= Logger.getLogger(EmaillicenceApplication.class.getName());	
 	public static void main(String[] args) {
+		System.out.println("start");
+		log.debug("email Debug message");
+		log.info("email Info message");
+		
 		SpringApplication.run(EmaillicenceApplication.class, args);
+		System.out.println("end");
 	}
 
 }
